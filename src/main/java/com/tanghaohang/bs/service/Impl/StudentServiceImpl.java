0c2020d6ceaccef20ca86bcf80cdcfa05616df07
thh <67777921+dingjianshuaige@users.noreply.github.com>
2022-06-26 13:15:26 +0800

    github commit

--- src/main/java/com/tanghaohang/bs/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/tanghaohang/bs/service/Impl/StudentServiceImpl.java
@@ -39,8 +39,8 @@ public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> impl
             //e.printStackTrace();
             log.error("发生错误。");
             //throw e;
-            throw new RuntimeException();
-            //TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            //throw new RuntimeException();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
         }
 
         //开启事务后发生异常会自动回滚，但是如果将语句用try/catch处理后就不会自动回滚，要么不使用try/catch，
